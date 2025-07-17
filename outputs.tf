60324513847958219bc5a2f39465b1fc7562a9a4
kaimmej <kaimmej@gmail.com>
2025-07-17 16:33:16 -0700

    removed the output text for the docker image. Didnt work, it caused exit with error code.

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "ec2-ip" {
   value       = aws_instance.minimal-instance.public_ip
   description = "My instance's ip"
 }
-
-output "docker-image-name" {
-  value       = aws_ecs_task_definition.my_task_definition.container_definitions[0].image
-  description = "The Docker image name that the EC2 container will pull down"
-}
