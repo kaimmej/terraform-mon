a95fadfd1fc0f2063e33cc0139b2b2664c8e4689
kaimmej <kaimmej@gmail.com>
2025-07-24 15:54:30 -0700

    Adding an additional public key to the EC2 instance

--- ec2.tf
+++ ec2.tf
@@ -4,8 +4,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_key_pair" "public" {
+resource "aws_key_pair" "public_Jon" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkI8MhSUcuCGQymb1cTzGwLs37biKFRB70h5WQkvesoS5mKXyAK3h4uPIThRxhI5Ehw5WJ++kCTBuMN0z3BJmXTQmZgxOPiORjUJULe9ELI4rZxoLsgBtY8ytaReI/P9jGHsKkPx2ebmwF7E+rZ6JHdOGRNLl0N7lc/XxOpOzxRPfDSfnydDoD9e+J+q3xh6DMD1gKGH2A3YrAl697lNm9BpmCkpSoAONE5nfjzbqn3KYjNF3ITQPZ/1XcJtAmWgwckLMa1yQdjoLUS3zpHSJc0ABCMf8IUQIJ/DDD2yIusqeoe4RGne1s3x64RlrPdBEB0NoGx4f5CmsSOeEiIHF1"
+}
+resource "aws_key_pair" "public_Ben" {
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOwp+5sSt53+244HeH6l1ZCM+jsEX3woB+K8Noi1eenZ"
 }
 
 resource "aws_iam_instance_profile" "ec2-role" {
@@ -17,12 +20,18 @@ resource "aws_iam_instance_profile" "ec2-role" {
 resource "aws_instance" "minimal-instance" {
   ami                         = "ami-0738bc713c45160fd"
   instance_type               = "t4g.small"
-  key_name                    = aws_key_pair.public.key_name
+  key_name                    = aws_key_pair.public_Jon.key_name
+  key_name                    = aws_key_pair.public_Ben.key_name
   vpc_security_group_ids      = [aws_security_group.open.id]
   user_data_replace_on_change = true
   iam_instance_profile        = aws_iam_role.ec2-role.name
   user_data                   = <<EOF
 #!/bin/bash
+
+# Adding Ben's public key to the instance. 
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOwp+5sSt53+244HeH6l1ZCM+jsEX3woB+K8Noi1eenZ" >> /home/ec2-user/.ssh/authorized_keys
+
+# DOCKER
 service docker start
 export PG_PASSWORD=$(aws secretsmanager get-secret-value --secret-id postgres-master-user --query SecretString)
 
