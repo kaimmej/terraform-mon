46effea9edaccbd0f659bdc59ebe5f5ce09251c7
kaimmej <kaimmej@gmail.com>
2025-07-24 16:37:45 -0700

    added gunicorn and nginx to work as our server and reverse proxy. Testing.

--- ec2.tf
+++ ec2.tf
@@ -27,31 +27,57 @@ resource "aws_instance" "minimal-instance" {
   user_data                   = <<EOF
 #!/bin/bash
 
+# 
 # Adding Ben's public key to the instance. 
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOwp+5sSt53+244HeH6l1ZCM+jsEX3woB+K8Noi1eenZ" >> /home/ec2-user/.ssh/authorized_keys
 
+# 
 # DOCKER
 service docker start
 export PG_PASSWORD=$(aws secretsmanager get-secret-value --secret-id postgres-master-user --query SecretString)
-
 # docker run -d -p 80:80 -e PG_PASSWORD=$${PG_PASSWORD} -e PG_USER=master nginx
 
+# 
+# NGINX
+# Write the nginx.conf file to the instance
+# This configuration sets up NGINX to reverse proxy requests to the Django application.
+cat << 'NGINXCONF' > /home/ec2-user/nginx.conf
+events {}
+
+http {
+    server {
+        listen 80;
+
+        location / {
+            proxy_pass http://app:8000;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }
+    }
+}
+NGINXCONF
+
+# 
+# DOCKER COMPOSE
 # Write the docker-compose file to the instance
 cat << 'COMPOSE' > /home/ec2-user/docker-compose-dockermon.yml
 name: dockermon
 version: '3.8'
 services :
 
+
+  # Django application service.
+  # Gunicorn is used as the WSGI server.
   app:
     build:
       context: .
       dockerfile: Dockerfile
     image: kaimmej/django_dockermon:latest
     container_name: docker-container-pokedex
-    ports:
-      - "80:8000"
-    volumes:
-      - ./parentProject-dockermon:/app
+    command: gunicorn dockermon.wsgi:application --bind 0.0.0.0:8000 --workers 3
+      ports:
+        - "8000:8000"
 
   db:
     image: postgres:latest
@@ -62,9 +88,22 @@ services :
       POSTGRES_DB: my_database
     ports:
       - "5432:5432"
+  
+  # NGINX reverse proxy setup.
+  nginx:
+    image: nginx:latest
+    container_name: nginx-proxy
+    ports:
+      - "80:80"
+    volumes:
+      - ./nginx.conf:/etc/nginx/nginx.conf
+    depends_on:
+      - app
+
 COMPOSE
 
-# Run docker-compose
+# RUN DOCKER-COMPOSE
+docker-compose -f /home/ec2-user/docker-compose-dockermon.yml up -d
 
 EOF
 }
