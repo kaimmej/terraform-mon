cfe17be32dd5359aff145c93657cf16a8963a550
kaimmej <kaimmej@gmail.com>
2025-07-31 15:03:01 -0700

    changes made to user-data

--- services/web-server/user-data.sh
+++ services/web-server/user-data.sh
@@ -7,36 +7,13 @@ echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOwp+5sSt53+244HeH6l1ZCM+jsEX3woB+K8No
 # 
 # DOCKER
 service docker start
-export PG_PASSWORD=$(aws secretsmanager get-secret-value --secret-id postgres-master-user --query SecretString)
-# docker run -d -p 80:80 -e PG_PASSWORD=$${PG_PASSWORD} -e PG_USER=master nginx
 
-# 
-# NGINX
-# Write the nginx.conf file to the instance
-# This configuration sets up NGINX to reverse proxy requests to the Django application.
-cat << 'NGINXCONF' > /home/ec2-user/nginx.conf
-events {}
-
-http {
-  server {
-    listen 80;
-
-    location / {
-      proxy_pass http://app:${server_port};
-      proxy_set_header Host $host;
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    }
-  }
-}
-NGINXCONF
 
 # 
 # DOCKER COMPOSE
 # Write the docker-compose file to the instance
 cat << 'COMPOSE' > /home/ec2-user/compose.yml
 name: dockermon
-version: '3.8'
 services:
 
 
@@ -60,17 +37,6 @@ services:
     ports:
       - "5432:5432"
   
-  # NGINX reverse proxy setup.
-#   nginx:
-#     image: nginx:latest
-#     container_name: nginx-proxy
-#     ports:
-#       - "80:80"
-#     volumes:
-#       - ./nginx.conf:/etc/nginx/nginx.conf
-#     depends_on:
-#       - app
-
 COMPOSE
 
 # Pull the latest Docker image for the Django application
