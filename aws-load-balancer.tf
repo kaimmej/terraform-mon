08b4f789a5c3a022461026dc293a5bec751905ed
kaimmej <kaimmej@gmail.com>
2025-08-05 14:44:05 -0700

    disabled http traffic to the loadbalancer, it now redirects to HTTPS

--- aws-load-balancer.tf
+++ aws-load-balancer.tf
@@ -30,8 +30,13 @@ resource "aws_lb_listener" "http" {
     protocol          = "HTTP"
 
     default_action {
-        type             = "forward"
-        target_group_arn = aws_lb_target_group.app.arn
+        type = "redirect"
+
+        redirect {
+            protocol = "HTTPS"
+            port     = "443"
+            status_code = "302"
+        }
     }
 }
 resource "aws_lb_listener" "https" {
